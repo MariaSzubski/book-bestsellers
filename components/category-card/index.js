374faf625600922804a97678c2169158d4a80354
Maria Szubski <hello@mariaszubski.com>
2022-10-28 19:50:51 -0400

    Fix console warnings

--- components/category-card/index.js
+++ components/category-card/index.js
@@ -1,15 +1,18 @@
-import Image from "next/image"
 import styles from "./styles.module.scss"
 
-const CategoryCard = ({ content }) => (
-  <a href={`/books/${content.list_name_encoded}`} className={styles.category_card}>
-    <img
-      src={content.books[0].book_image}
-      className={styles.hero}
-      alt={`Book Cover: ${content.books[0].title} ${content.books[0].contributer}`}
-    />
-    <h2>{content.display_name} &rarr;</h2>
-  </a>
-)
+const CategoryCard = ({ content }) => {
+  const top_book = content.books[0]
+
+  return (
+    <a href={`/books/${content.list_name_encoded}`} className={styles.category_card}>
+      <img
+        src={top_book.book_image}
+        className={styles.hero}
+        alt={`Book Cover: ${top_book.title} ${top_book.contributer}`}
+      />
+      <h2>{content.display_name} &rarr;</h2>
+    </a>
+  )
+}
 
 export default CategoryCard
